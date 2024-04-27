79b18f55f0a5b89f3aebf444db9081f9054e4559
Kisan <kisankumavat85@gmail.com>
2024-04-27 16:00:59 +0530

    set runtime as edge

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,8 @@ import { redirect } from "next/navigation";
 import { auth } from "@/auth";
 import Dashboard from "@/components/pages/Dashboard";
 
+export const runtime = "edge";
+
 const DashboardPage = async () => {
   const session = await auth();
   if (!session?.user?.id) return redirect("/");
