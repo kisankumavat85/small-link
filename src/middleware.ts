75a64382ea3c3cf507e7b8559e75377fa60cfe1b
Kisan <kisankumavat85@gmail.com>
2024-05-09 22:58:51 +0530

    fix: Add matcher in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,4 +16,8 @@ const middleware = async (request: NextRequest) => {
   }
 };
 
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
+
 export default middleware;
